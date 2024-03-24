8760476e5a7b42c32daebf8f2da41ea001962cce
Lionel <gitlionel@gmail.com>
2024-03-24 18:43:14 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var variable = "Hello World";
+var number = 0;
+var personas = ["Juan", "Pedro", "Maria"];
+console.log(variable, "-", number);
+personas.map(function (persona) {
+    console.log(persona);
+});
---
+++ main.ts
@@ -0,0 +1,9 @@
+let variable:string = "Hello World";
+let number:number = 0;
+let personas:string[] = ["Juan", "Pedro", "Maria"];
+
+console.log(variable, "-",number);
+
+personas.map((persona:string) => {
+    console.log(persona);
+});
